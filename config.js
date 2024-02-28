97bd7127bba2782300a23057cfa8c054f789f62d
abdallah <Abdallah@local.com>
2024-02-28 07:42:39 +0200

    abdallah

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import pg from "pg";
 const pool = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
+  user: "abdallah",
+  host: "dpg-cn9p3rv79t8c73c51vd0-a.oregon-postgres.render.com",
   database: "complains",
-  password: "11620000",
+  password: "NjAo5XQyVpWJNz2KCMPIMDktWzQOLizx",
   port: 5432,
+  max: 20, // Adjust as needed
+  idleTimeoutMillis: 30000, // Adjust as needed
+  ssl: {
+    // These options are often required for connecting to a remote PostgreSQL server
+    // Adjust them based on your server's configuration
+    rejectUnauthorized: false,
+  },
 });
 export const secret = "abch1525wd";
 
